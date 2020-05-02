c0ff3bb8c831488b7c3f5d7750ab5c23c62761fc
Subhed Chavan <sachavan@mail.csuchico.edu>
2020-05-02 12:02:40 -0700

    Adding Dashboard

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,29 @@ var router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {  
-  if (!req.isAuthenticated()) {
-    let errors = [];
-    res.render('index', { errors });
-  }
-  else{
-    res.redirect('/home', { title: 'Express' });
-  }
+router.get('/', function(req, res, next) {
+    if (!req.isAuthenticated()) {
+        let errors = [];
+        res.render('index', { errors });
+    } else {
+        res.redirect('/home', { title: 'Express' });
+    }
 
 });
 
-router.get('/home', ensureAuthenticated, function (req, res){
-  if (!req.isAuthenticated()) {
-    let errors = [];
-    res.redirect('index', { errors });
-  }
-  else{
-  res.render('dashboard', { 
-    name: req.user.name  
-  })
-  }
+router.get('/home', ensureAuthenticated, function(req, res) {
+    if (!req.isAuthenticated()) {
+        let errors = [];
+        res.redirect('index', { errors });
+    } else {
+        res.render('dashboard', {
+            name: req.user.name
+        })
+    }
 
 });
+
+
 
 
 module.exports = router;
