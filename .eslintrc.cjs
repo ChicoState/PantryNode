5016f7cf7c47749f26dcd7ccc617095b5f950e89
Reema <reema.a.shadid@gmail.com>
2023-03-06 19:24:34 -0800

    fixed linter configuration for proper run

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,18 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'react-app'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
     rules: {
-        "@typescript-eslint/no-var-requires": "error",
+        "no-var-requires": "off",
+        "@typescript-eslint/no-var-requires": "warn",
         "no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": "warn",
         "no-redeclare": "off",
         "@typescript-eslint/no-redeclare": "warn",
         "no-extra-semi": "off",
-        "@typescript-eslint/no-extra-semi": "error"
+        "@typescript-eslint/no-extra-semi": "warn",
+        "no-undef": "off",
+        "@typescript-eslint/no-undef": "warn"
     }
-     
 };
