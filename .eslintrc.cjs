7d65fe453d6f8f0fcf9eb7014e0c7d4d36d77232
Reema <reema.a.shadid@gmail.com>
2023-03-06 20:24:35 -0800

    cleaned up rule duplicates, issue #19

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,8 @@ module.exports = {
     plugins: ['@typescript-eslint'],
     root: true,
     rules: {
-        "no-var-requires": "off",
         "@typescript-eslint/no-var-requires": "warn",
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "warn",
-        "no-redeclare": "off",
-        "@typescript-eslint/no-redeclare": "warn",
-        "no-extra-semi": "off",
         "@typescript-eslint/no-extra-semi": "warn",
-        "no-undef": "off",
         "no-undef": "warn"
     }
 };
