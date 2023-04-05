9261fcd7a02e646154d3799cba5304764a52cfad
hardikpatil <hardik.patil2494@gmail.com>
2023-04-05 10:40:26 -0700

    Feed component resolves #76

--- PantryNodeReact/src/pages/index.tsx
+++ PantryNodeReact/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Grid, Card, CardContent, Container, Typography } from '@mui/material';
+import Feed from "../Components/Feed/Feed";
 
 const index = () => {
 
@@ -24,46 +24,7 @@ const index = () => {
   });
 
   return (
-
-    <Box sx={{ width: '100%', bgcolor: '', p: 2 }}>
-      <Typography variant="h4" gutterBottom style={{ textAlign: "center" }}>
-        Feed
-      </Typography>
-
-      <div style={{ display: "flex", flexWrap:"wrap", justifyContent: "space-between", alignItems: "center",}}>
-
-        {sortedFeedList.map((item, index) => {
-          
-          const expiryDate = new Date(item.expiry_date).getTime();
-          const currentDate = new Date().getTime();
-          const isExpiring = expiryDate > currentDate && expiryDate - currentDate < 7 * 24 * 60 * 60 * 1000;
-          const isExpired = expiryDate <= currentDate;
-          return (
-            <div key={item.id} 
-                  style={{ 
-                    display: "flex",
-                    width:"48%",
-                    justifyContent: "space-between", 
-                    alignItems: "center", 
-                    padding: "25px",
-                    backgroundColor:"#eee8e8",
-                    margin: "10px",
-                    borderBottom: index !== sortedFeedList.length - 1 ? "1px solid #ccc" : "none" 
-                  }}>
-              
-                    <Typography>
-                      {item.item} {isExpiring ? "expiring soon" : isExpired ? "expired on" : "getting expiring on"} {item.expiry_date}
-                    </Typography>
-                    
-                    <Typography>
-                      <strong>Stock Quantity:{item.quantity}</strong>
-                    </Typography>
-            </div>
-          );
-        })}
-
-      </div>
-    </Box>
+    <Feed sortedFeedList={sortedFeedList} />
   );
 };
 
