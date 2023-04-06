9eee05f82e7880e600d6214703d1aea798290348
Kushal Ramakanth <kushal.r.96@gmail.com>
2023-04-06 13:14:27 -0700

    Added button and modal for form entry

--- PantryNodeReact/src/pages/donor.tsx
+++ PantryNodeReact/src/pages/donor.tsx
@@ -1,13 +1,80 @@
-import React from "react";
-import { Button, Table, TableBody, TableHead, TableRow, TableContainer, Paper, TableCell } from '@mui/material';
+import { useState } from 'react';
+import { Button, Table, TableBody, TableHead, TableRow, TableContainer, Paper, TableCell, DialogTitle, TextField, DialogContent, Dialog, DialogActions } from '@mui/material';
 
 // Dummy Data
 const data = [
   { name: 'John', email: 'john@gmail.com', location: 'USA' },
 ];
 
-var donor = () => {
+var Donor = () => {
+  interface Entry {
+    name: string;
+    email: string;
+    location: string;
+  }
+
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [data, setData] = useState<Entry[]>([]);
+  const [newEntry, setNewEntry] = useState<Entry>({ name: '', email: '', location: '' });
+
+  const handleAddEntry = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setData([...data, newEntry]);
+    setNewEntry({ name: '', email: '', location: '' });
+    setShowModal(false);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewEntry({ ...newEntry, [e.target.name]: e.target.value });
+  };
+
   return <div>
+
+    <Button variant="contained" color="primary" onClick={() => setShowModal(true)}>
+      Add Entry
+    </Button>
+
+    <Dialog open={showModal} onClose={() => setShowModal(false)}>
+        <DialogTitle>Add New Entry</DialogTitle>
+        <DialogContent>
+          <form onSubmit={handleAddEntry}>
+            <TextField
+              autoFocus
+              margin="dense"
+              label="Name"
+              name="name"
+              value={newEntry.name}
+              onChange={handleChange}
+              fullWidth
+            />
+            <TextField
+              margin="dense"
+              label="Email"
+              name="email"
+              value={newEntry.email}
+              onChange={handleChange}
+              fullWidth
+            />
+            <TextField
+              margin="dense"
+              label="Location"
+              name="location"
+              value={newEntry.location}
+              onChange={handleChange}
+              fullWidth
+            />
+            <DialogActions>
+              <Button onClick={() => setShowModal(false)} color="primary">
+                Cancel
+              </Button>
+              <Button type="submit" color="primary">
+                Add
+              </Button>
+            </DialogActions>
+          </form>
+        </DialogContent>
+      </Dialog>
+
     <TableContainer component={Paper} style={{ marginTop: '1rem' }}>
       <Table>
         <TableHead>
@@ -40,4 +107,4 @@ var donor = () => {
   </div>;
 };
 
-export default donor;
+export default Donor;
