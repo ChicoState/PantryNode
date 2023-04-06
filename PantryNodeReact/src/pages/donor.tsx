7cd28d1799beb5f228de3bde43d873a19ad66c6c
Kushal Ramakanth <kushal.r.96@gmail.com>
2023-04-06 13:35:47 -0700

    Performed Table sort on header click, resolved #77

--- PantryNodeReact/src/pages/donor.tsx
+++ PantryNodeReact/src/pages/donor.tsx
@@ -1,11 +1,6 @@
 import { useState } from 'react';
 import { Button, Table, TableBody, TableHead, TableRow, TableContainer, Paper, TableCell, DialogTitle, TextField, DialogContent, Dialog, DialogActions } from '@mui/material';
 
-// Dummy Data
-const data = [
-  { name: 'John', email: 'john@gmail.com', location: 'USA' },
-];
-
 var Donor = () => {
   interface Entry {
     name: string;
@@ -13,13 +8,20 @@ var Donor = () => {
     location: string;
   }
 
+  interface SortConfig {
+    key: keyof Entry | null;
+    direction: 'ascending' | 'descending' | null;
+  }
+
   const initialData: Entry[] = [
     { name: 'John', email: 'john@gmail.com', location: 'USA' },
+    { name: 'Danny', email: 'danny@gmail.com', location: 'USA' },
   ];
-  
+
   const [showModal, setShowModal] = useState<boolean>(false);
   const [data, setData] = useState<Entry[]>(initialData);
   const [newEntry, setNewEntry] = useState<Entry>({ name: '', email: '', location: '' });
+  const [sortConfig, setSortConfig] = useState<SortConfig>({ key: null, direction: null });
 
   const handleAddEntry = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -32,6 +34,32 @@ var Donor = () => {
     setNewEntry({ ...newEntry, [e.target.name]: e.target.value });
   };
 
+  const onSort = (key: keyof Entry) => {
+    let direction: 'ascending' | 'descending' = 'ascending';
+    if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
+
+  const sortedData = (): Entry[] => {
+    const sortedData = [...data];
+    if (sortConfig !== null) {
+      sortedData.sort((a: Entry, b: Entry) => {
+        if (sortConfig.key !== null) {
+          if (a[sortConfig.key] < b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? -1 : 1;
+          }
+          if (a[sortConfig.key] > b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? 1 : -1;
+          }
+        }
+        return 0;
+      });
+    }
+    return sortedData;
+  };
+
   return <div>
 
     <Button variant="contained" color="primary" onClick={() => setShowModal(true)}>
@@ -39,60 +67,76 @@ var Donor = () => {
     </Button>
 
     <Dialog open={showModal} onClose={() => setShowModal(false)}>
-        <DialogTitle>Add New Entry</DialogTitle>
-        <DialogContent>
-          <form onSubmit={handleAddEntry}>
-            <TextField
-              autoFocus
-              margin="dense"
-              label="Name"
-              name="name"
-              value={newEntry.name}
-              onChange={handleChange}
-              fullWidth
-            />
-            <TextField
-              margin="dense"
-              label="Email"
-              name="email"
-              value={newEntry.email}
-              onChange={handleChange}
-              fullWidth
-            />
-            <TextField
-              margin="dense"
-              label="Location"
-              name="location"
-              value={newEntry.location}
-              onChange={handleChange}
-              fullWidth
-            />
-            <DialogActions>
-              <Button onClick={() => setShowModal(false)} color="primary">
-                Cancel
-              </Button>
-              <Button type="submit" color="primary">
-                Add
-              </Button>
-            </DialogActions>
-          </form>
-        </DialogContent>
-      </Dialog>
+      <DialogTitle>Add New Entry</DialogTitle>
+      <DialogContent>
+        <form onSubmit={handleAddEntry}>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Name"
+            name="name"
+            value={newEntry.name}
+            onChange={handleChange}
+            fullWidth
+          />
+          <TextField
+            margin="dense"
+            label="Email"
+            name="email"
+            value={newEntry.email}
+            onChange={handleChange}
+            fullWidth
+          />
+          <TextField
+            margin="dense"
+            label="Location"
+            name="location"
+            value={newEntry.location}
+            onChange={handleChange}
+            fullWidth
+          />
+
+          <DialogActions>
+            <Button onClick={() => setShowModal(false)} color="primary">
+              Cancel
+            </Button>
+            <Button type="submit" color="primary">
+              Add
+            </Button>
+          </DialogActions>
+        </form>
+      </DialogContent>
+    </Dialog>
 
     <TableContainer component={Paper} style={{ marginTop: '1rem' }}>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>No.</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Location</TableCell>
+            <TableCell onClick={() => onSort('name')}>
+              <strong>Name</strong>
+              {sortConfig && sortConfig.key === 'name' && (
+                sortConfig.direction === 'ascending' ? ' 🔼' : ' 🔽'
+              )}
+            </TableCell>
+            <TableCell onClick={() => onSort('email')}>
+              <strong>Email</strong>
+              {sortConfig && sortConfig.key === 'email' && (
+                sortConfig.direction === 'ascending' ? ' 🔼' : ' 🔽'
+              )}
+            </TableCell>
+            <TableCell onClick={() => onSort('location')}>
+              <strong>Location</strong>
+              {sortConfig && sortConfig.key === 'location' && (
+                sortConfig.direction === 'ascending' ? ' 🔼' : ' 🔽'
+              )}
+            </TableCell>
             <TableCell>Donation</TableCell>
           </TableRow>
         </TableHead>
 
         <TableBody>
-          {data.map((entry, index) => (
+          {sortedData().map((entry, index) => (
             <TableRow key={index}>
               <TableCell>{index + 1}</TableCell>
               <TableCell>{entry.name}</TableCell>
