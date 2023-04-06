84a3fd736bd7475ef638f7fe158f9439b3bf4eb6
Kushal Ramakanth <kushal.r.96@gmail.com>
2023-04-06 12:51:47 -0700

    Added Donar List Page

--- PantryNodeReact/src/pages/donor.tsx
+++ PantryNodeReact/src/pages/donor.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {Table, TableBody, TableHead, TableRow, TableContainer, Paper, TableCell} from '@mui/material';
 
-const donor = () => {
-  return <div>donor</div>;
+// Dummy Data
+const data = [
+  {name: 'John', email: 'john@gmail.com', location: 'USA'},
+];
+
+var donor = () => {
+  return <div>
+      <TableContainer component={Paper} style={{ marginTop: '1rem' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>No.</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Location</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {data.map((entry, index) => (
+              <TableRow key={index}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>{entry.name}</TableCell>
+                <TableCell>{entry.email}</TableCell>
+                <TableCell>{entry.location}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+  </div>;
 };
 
 export default donor;
