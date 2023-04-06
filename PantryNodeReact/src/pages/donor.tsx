8c78445818561a09b1268133b950e1282ec6b285
Kushal Ramakanth <kushal.r.96@gmail.com>
2023-04-06 12:57:52 -0700

    Added Donate Button for each entry

--- PantryNodeReact/src/pages/donor.tsx
+++ PantryNodeReact/src/pages/donor.tsx
@@ -1,36 +1,42 @@
 import React from "react";
-import {Table, TableBody, TableHead, TableRow, TableContainer, Paper, TableCell} from '@mui/material';
+import { Button, Table, TableBody, TableHead, TableRow, TableContainer, Paper, TableCell } from '@mui/material';
 
 // Dummy Data
 const data = [
-  {name: 'John', email: 'john@gmail.com', location: 'USA'},
+  { name: 'John', email: 'john@gmail.com', location: 'USA' },
 ];
 
 var donor = () => {
   return <div>
-      <TableContainer component={Paper} style={{ marginTop: '1rem' }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>No.</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Location</TableCell>
-            </TableRow>
-          </TableHead>
+    <TableContainer component={Paper} style={{ marginTop: '1rem' }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>No.</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Location</TableCell>
+            <TableCell>Donation</TableCell>
+          </TableRow>
+        </TableHead>
 
-          <TableBody>
-            {data.map((entry, index) => (
-              <TableRow key={index}>
-                <TableCell>{index + 1}</TableCell>
-                <TableCell>{entry.name}</TableCell>
-                <TableCell>{entry.email}</TableCell>
-                <TableCell>{entry.location}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+        <TableBody>
+          {data.map((entry, index) => (
+            <TableRow key={index}>
+              <TableCell>{index + 1}</TableCell>
+              <TableCell>{entry.name}</TableCell>
+              <TableCell>{entry.email}</TableCell>
+              <TableCell>{entry.location}</TableCell>
+              <TableCell>
+                <Button variant="contained" color="primary" onClick={() => alert(`Donate button clicked for ${entry.name}`)}>
+                  Donate
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>;
 };
 
