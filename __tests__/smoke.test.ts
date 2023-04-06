22483c82432eb28440e37dceaf45de80c29d5daf
Brian S Wells <briswells@gmail.com>
2023-04-06 11:33:09 -0700

    resolving merge errors

--- __tests__/smoke.test.ts
+++ __tests__/smoke.test.ts
@@ -4,7 +4,11 @@ describe('smoke test', () => {
   });
 
   test('can import entry point without errors', () => {
+<<<<<<< HEAD
     expect(() => require('../routes/index.js')).not.toThrow();
+=======
+    expect(() => require('../backend/routes/index.js')).not.toThrow();
+>>>>>>> de5bac2 (resolving merge errors)
   });
   
 });
