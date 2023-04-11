a00ecd48e46a949cef645a47d5c4c0b2082b27c0
David Coles <davidepcoles@gmail.com>
2023-04-11 21:49:54 +0000

    Tables configured for mobile, interfaces and temp data started

--- PantryNodeReact/src/pages/summary.tsx
+++ PantryNodeReact/src/pages/summary.tsx
@@ -9,13 +9,54 @@ import {
   Paper,
   TableCell,
   Grid,
-  DialogTitle,
-  TextField,
-  DialogContent,
-  Dialog,
-  DialogActions,
 } from "@mui/material";
 var summary = () => {
+  interface purchaseInterface {
+    name: string;
+    type: string;
+    quantity: number;
+    date: string;
+  }
+
+  interface currentStockInterface  {
+    name: string;
+    type: string;
+    stockedDate: string;
+    expiryDate: string;
+  }
+
+  interface wasteManagementInterface  {
+    name: string;
+    quantity: number;
+    date: string;
+  }
+
+  interface expiryInterface  {
+    name: string;
+    daysRemaining: number;
+    date: string;
+  }
+
+  const initialDataPurchase: purchaseInterface[] = [
+    { name: "John", type: "Food", quantity: 1, date: "2021-10-10" },
+    { name: "Danny", type: "Food", quantity: 1, date: "2021-10-10" },
+  ];
+
+  const initialDataCurrentStock: currentStockInterface[] = [
+    { name: "John", type: "Food", stockedDate: "2021-10-10", expiryDate: "2021-10-10" },
+    { name: "Danny", type: "Food", stockedDate: "2021-10-10", expiryDate: "2021-10-10" },
+  ];
+
+  const initialDataWasteManagement: wasteManagementInterface[] = [
+    { name: "John", quantity: 1, date: "2021-10-10" },
+    { name: "Danny", quantity: 1, date: "2021-10-10" },
+  ];
+
+  const initialDataExpiry: expiryInterface[] = [
+    { name: "John", daysRemaining: 1, date: "2021-10-10" },
+    { name: "Danny", daysRemaining: 1, date: "2021-10-10" },
+  ];
+
   return (
     <div>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
