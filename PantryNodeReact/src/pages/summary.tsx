e7057a7050e1e35c5f380f8767c27cef530b036c
David Coles <davidepcoles@gmail.com>
2023-04-11 20:51:15 +0000

    Inital table configuration started

--- PantryNodeReact/src/pages/summary.tsx
+++ PantryNodeReact/src/pages/summary.tsx
@@ -1,7 +1,128 @@
-import React from "react";
-
-const summary = () => {
-  return <div>summary</div>;
+import { useState } from "react";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableContainer,
+  Paper,
+  TableCell,
+  Grid,
+  DialogTitle,
+  TextField,
+  DialogContent,
+  Dialog,
+  DialogActions,
+} from "@mui/material";
+var summary = () => {
+  return (
+    <div>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={10} md={6}>
+          <strong>Purchases</strong>
+          <br />
+          Total Checkout: 0 | Total Quantity Checked Out: 0{/*Purchase Table*/}
+          <TableContainer component={Paper} style={{ marginTop: "1rem" }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>#</TableCell>
+                  <TableCell>
+                    <strong>Name</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Type</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Quantity</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Date</strong>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={10} md={6}>
+        <strong>Current Stock</strong>
+          <br />
+          Total Items in Stock: 0
+          {/*Current Stock Table*/}
+          <TableContainer component={Paper} style={{ marginTop: "1rem" }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>#</TableCell>
+                  <TableCell>
+                    <strong>Name</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Type</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Stocked Date</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Expiry Date</strong>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={10} md={6}>
+          <strong>Waste Management</strong>
+          <br />
+          Total Expired: 0
+          {/*Waste Management Table*/}
+          <TableContainer component={Paper} style={{ marginTop: "1rem" }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>#</TableCell>
+                  <TableCell>
+                    <strong>Name</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Quantity</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Date</strong>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={10} md={6}>
+        <strong>Soon to Expire</strong>
+          <br />
+          <br />
+          {/*Soon to Expire Table*/}
+          <TableContainer component={Paper} style={{ marginTop: "1rem" }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>#</TableCell>
+                  <TableCell>
+                    <strong>Name</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Days Remaining</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Date</strong>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default summary;
