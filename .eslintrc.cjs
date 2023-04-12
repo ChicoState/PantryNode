909f8aa85d7ef29dbaff3ac21256d5923aa3e5b6
Anoushka Sharma <asharma7@csuchico.edu>
2023-04-12 00:38:06 -0700

    Fixed issueundef error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,15 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'react-app'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-    rules: {
-        "@typescript-eslint/no-var-requires": "warn",
-        "@typescript-eslint/no-extra-semi": "warn",
-    },
-
-    
-      "ignorePatterns": ["eslintrc.js"],
-        //add rules
-
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "react-app",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-var-requires": "warn",
+  },
+  "ignorePatterns": ["eslintrc.js"],
+    //add rules
 };
