31e2c38b224a6505fea68360d8c9d86c68503e02
Abhilash Sreenivasa <abhilash.sreenivasa@gmail.com>
2023-04-12 18:38:46 -0700

    Bar chart fuctionality has been added #91

--- PantryNodeReact/src/Components/Summary/BarChart.tsx
+++ PantryNodeReact/src/Components/Summary/BarChart.tsx
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer  } from 'recharts';
 
 interface DataProps {
   name: string;
@@ -10,11 +11,25 @@ interface BarChartProps {
 
 }
 
-function BarChart(props: BarChartProps) { 
-
+function SummaryBarChart(props: BarChartProps) {
+  const { data } = props;
+  
   return (
-    <h1>Chart</h1>
+    <>
+    <ResponsiveContainer width="100%" height={300}>
+    <BarChart width={500} height={300} data={data} >
+      <XAxis dataKey="name" />
+      <YAxis label={{ value: 'Quantity',angle: -90, position: 'insideLeft' }} />
+      <CartesianGrid stroke="#ccc" />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="value" fill="#8C2332" />
+    </BarChart>
+    </ResponsiveContainer>
+   
+   
+    </>
   );
 }
 
-export default BarChart;
+export default SummaryBarChart;
--- PantryNodeReact/src/pages/summary.tsx
+++ PantryNodeReact/src/pages/summary.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import  BarChart from "../Components/Summary/BarChart";
 import {
   Table,
   TableBody,
