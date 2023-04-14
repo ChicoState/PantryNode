1dcf5cba177de2b9d1b5198223a451f10d961538
Hardik Patil <hardik.patil2494@gmail.com>
2023-04-13 22:32:11 -0700

    Linter error on frontend (#150)
    * fix lint error on indent. resolved #147
    
    * resolved console error on donor page
    
    * Update actions.yml to handle PR to frontend
    
    * Update linter.yml to handle PR to frontend
    
    * changed off to warn: no-explicit-any and no-unused-vars
    
    * changed CI to false to not consider lint warns as errors
    
    * changed CI to false in package.json
    
    * added names to run commands in actions.yml
    
    * added names to run commands in actions.yml v2
    
    ---------
    
    Co-authored-by: Jay Revolinsky <98365370+jayrevolinskyjr@users.noreply.github.com>
    Co-authored-by: AbhinavReddy-Dev <abhinavreddy9681@gmail.com>

--- PantryNodeReact/src/Components/Summary/BarChart.tsx
+++ PantryNodeReact/src/Components/Summary/BarChart.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer  } from 'recharts';
+import React from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 interface DataProps {
   name: string;
@@ -8,26 +8,23 @@ interface DataProps {
 
 interface BarChartProps {
   data: DataProps[];
-
 }
 
 function SummaryBarChart(props: BarChartProps) {
   const { data } = props;
-  
+
   return (
     <>
-    <ResponsiveContainer width="100%" height={300}>
-    <BarChart width={500} height={300} data={data} >
-      <XAxis dataKey="name" />
-      <YAxis label={{ value: 'Quantity',angle: -90, position: 'insideLeft' }} />
-      <CartesianGrid stroke="#ccc" />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="value" fill="#8C2332" />
-    </BarChart>
-    </ResponsiveContainer>
-   
-   
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data}>
+          <XAxis dataKey="name" />
+          <YAxis label={{ value: "Quantity", angle: -90, position: "insideLeft" }} />
+          <CartesianGrid stroke="#ccc" />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="value" fill="#8C2332" />
+        </BarChart>
+      </ResponsiveContainer>
     </>
   );
 }
