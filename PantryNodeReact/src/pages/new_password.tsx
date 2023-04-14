d8a51ae4c0d244513e00c60f3626d6d23dc19a37
Sachin Prasad <91291081+sachinprasad1998mav@users.noreply.github.com>
2023-04-13 22:09:14 -0700

    changes made to forgotPassword feature

--- PantryNodeReact/src/pages/new_password.tsx
+++ PantryNodeReact/src/pages/new_password.tsx
@@ -3,69 +3,123 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
+// import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
+import InputAdornment from '@mui/material/InputAdornment';
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { useNavigate } from "react-router-dom";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Copyright from '../Components/Copyright';
 import { useState } from "react";
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '1px solid ',
+  borderColor:"primary",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Passwordreset(){
-
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
   const [npassword, setNpassword] = useState("");
   const [npasswordError, setNpasswordError] = useState("");
   const [cnpassword, setCNpassword] = useState("");
   const [cnpasswordError, setCNpasswordError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+  const [showNPassword, setShowNPassword] = useState(false);
+  const [showCNPassword, setShowCNPassword] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     if (name === "npassword") {
       setNpassword(value);
-      setNpasswordError("");
+      if (value.trim() === "") {
+        setNpasswordError("Password is required");
+      } else if (value.length < 8) {
+        setNpasswordError("Password must be at least 8 characters long");
+      } else if (!/(?=.*[A-Z])(?=.*[\W_])/.test(value)) {
+        setNpasswordError(
+          "Password must contain at least one uppercase, one number and one special character");
+      }
+      else{
+        setNpasswordError("");
+      }
     }
     if (name === "cnpassword") {
       setCNpassword(value);
-      setCNpasswordError("");
+      if(npassword!==value){
+      setCNpasswordError("New password and confirm password do not match");
+      }
+      else
+      {
+        setCNpasswordError("");
+        setIsDisabled(false);
+      }
+    }
+  };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  
+  //   if (npassword !== cnpassword) {
+  //     alert("New password and confirm password do not match.");
+  //     return;
+  //   }
+  
+  // }
+  
+
+  const handleToggleNPasswordVisibility = () => {
+    setShowNPassword(!showNPassword);
+  };
+  
+  const handleToggleCNPasswordVisibility = () => {
+    setShowCNPassword(!showCNPassword);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    setError(false);
+  };
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
+  const handleClick = () => {
+    if (password === '') {
+      setError(true);
+    } else {
+      console.log('Password:', password);
     }
   };
 
 
-  const navigate = useNavigate();
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const npasswordValue = data.get("npassword") as string;
-    const cnpasswordValue = data.get("cnpassword") as string;
-    // npassword
-    if (npasswordValue.trim() === "") {
-      setNpasswordError("Password is required");
-      return;
-    }
-    if (npasswordValue.length < 8){
-      setNpasswordError("Password must be at least 8 characters long");
-      return;
-    }
-    if (!/(?=.*[A-Z])(?=.*[\W_])/.test(npasswordValue)) {
-      setNpasswordError("Password must contain at least one number and one special character");
-      return;
-    }
-    //confirm password
-    if (cnpasswordValue.trim() === "") {
-      setCNpasswordError("Password is required");
-      return;
-    }
-    if (cnpasswordValue.length < 8){
-      setCNpasswordError("Password must be at least 8 characters long");
-      return;
-    }
-    if (!/(?=.*[A-Z])(?=.*[\W_])/.test(cnpasswordValue)) {
-      setCNpasswordError("Password must contain at least one number and one special character");
-      return;
-    }
-    navigate("/passwordsuccessful")
+    console.log("Handle Submit");
+    setOpen(true);
+    // const data = new FormData(event.currentTarget);
+    // const npasswordValue = data.get("npassword") as string;
+    // const cnpasswordValue = data.get("cnpassword") as string;
+    // navigate("/passwordsuccessful")
   };
   return (
       <Container component="main" maxWidth="xs">
@@ -86,52 +140,95 @@ export default function Passwordreset(){
           </Typography>
           <p>Enter you new password</p>
           <Box component="form" onSubmit={handleSubmit}  sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="npassword"
-              label="New Password"
-              name="npassword"
-              autoComplete="npassword"
-              autoFocus
-              value={npassword}
-              onChange={handleChange}
-              error={Boolean(npasswordError)}
-              helperText={npasswordError}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="cnpassword"
-              label="Confirm New Password"
-              name="cnpassword"
-              autoComplete="cnpassword"
-              autoFocus
-              value={cnpassword}
-              onChange={handleChange}
-              error={Boolean(cnpasswordError)}
-              helperText={cnpasswordError}
-            />
+          
+          <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="npassword"
+          label="New Password"
+          name="npassword"
+          autoComplete="npassword"
+          autoFocus
+          type={showNPassword ? "text" : "password"} 
+          value={npassword}
+          onChange={handleChange}
+          error={Boolean(npasswordError)}
+          helperText={npasswordError}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleToggleNPasswordVisibility}>
+                  {showNPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+          
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="cnpassword"
+          label="Confirm New Password"
+          name="cnpassword"
+          autoComplete="cnpassword"
+          autoFocus
+          type={showCNPassword ? "text" : "password"} 
+          value={cnpassword}
+          onChange={handleChange}
+          error={Boolean(cnpasswordError)}
+          helperText={cnpasswordError}
+          
+        />
+
+
             <Button
               type="submit"
               fullWidth
+              disabled = {isDisabled}
+              variant="contained"
+              sx={{ mt: 3, mb: 2, py:2 }}
+              style={{
+                backgroundColor: "primary"
+              }}
+              // onClick={handleSubmit}
+            >
+              Set New Password
+            </Button>
+            
+          </Box>
+        </Box>
+        {/* Modal code goes here */}
+        <Modal
+        open={open}
+        // onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2" >
+            Password Changed Successfully!
+          </Typography>
+
+          {/* <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography> */}
+          <Button
+              type="submit"
+              fullWidth
+              disabled = {isDisabled}
               variant="contained"
               sx={{ mt: 3, mb: 2, py:2 }}
               style={{
                 backgroundColor: "primary"
               }}
             >
-              Set New Password
+              Login
             </Button>
-            <Grid container>
-              <Grid item xs>
-              </Grid>
-              
-            </Grid>
-          </Box>
         </Box>
+      </Modal>  
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
   );
