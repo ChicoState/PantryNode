471c3aad065b811bbc06d15dfa617ba06756a1a3
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-04-14 21:36:46 -0700

    changed scanner class comp to functional comp

--- frontend/src/pages/scanner.tsx
+++ frontend/src/pages/scanner.tsx
@@ -1,40 +1,39 @@
-import React, { Component } from 'react';
-import { Quagga } from 'quagga';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as React from "react";
+import { Quagga } from "quagga";
 
-class Scanner extends Component {
-  handleBarcodeScan = (data: any) => {
-    console.log('Barcode detected:', data.codeResult.code);
+const Scanner = () => {
+  const handleBarcodeScan = (data: any) => {
+    console.log("Barcode detected:", data.codeResult.code);
     // Do something with the barcode data
   };
 
-  handleBarcodeError = (error: any) => {
-    console.error('Barcode error:', error.message);
+  const handleBarcodeError = (error: any) => {
+    console.error("Barcode error:", error.message);
     // Handle any errors that occur during scanning
   };
 
-  render() {
-    return (
-      <div>
-        <h1>Barcode Scanner</h1>
-        <Quagga
-          onDetected={this.handleBarcodeScan}
-          onError={this.handleBarcodeError}
-          style={{ width: '100%', height: 'auto' }}
-          config={{
-            inputStream: {
-              name: 'Live',
-              type: 'LiveStream',
-              target: '#barcode-scanner'
-            },
-            decoder: {
-              readers: ['ean_reader']
-            }
-          }}
-        />
-        <div id="barcode-scanner"></div>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Barcode Scanner</h1>
+      <Quagga
+        onDetected={handleBarcodeScan}
+        onError={handleBarcodeError}
+        style={{ width: "100%", height: "auto" }}
+        config={{
+          inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: "#barcode-scanner",
+          },
+          decoder: {
+            readers: ["ean_reader"],
+          },
+        }}
+      />
+      <div id="barcode-scanner"></div>
+    </div>
+  );
+};
 
 export default Scanner;
