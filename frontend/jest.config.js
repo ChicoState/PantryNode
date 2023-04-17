65a29eed7e8093a7d3bae6e3958234bcf09f7839
chetan2298 <45332082+chetan2298@users.noreply.github.com>
2023-04-17 16:54:20 -0700

    Added Sample testcases for Frontend components

---
+++ frontend/jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|js|tsx|jsx)$': '@swc/jest',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+  ],
+
+};
