7426aaa36604803fbfb3a0560e466a89a665e1d0
jayrevolinskyjr <jrevolinsky@csuchico.edu>
2023-04-18 11:41:27 -0700

    Adjust barcode route to match SQL tables

--- backend/src/routes/barcode.js
+++ backend/src/routes/barcode.js
@@ -1,17 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-// Define the route GET function
-router.get('/barcode/:barcode_num', async (req, res) => {
-    const barcode_num = req.params.barcode_num;
-    const result = await item.findOne({ where: { barcode_num } });
+router.get('/barcode/:barcode', async (req, res) => {
+    const barcode_var = req.params.barcode;
+    const result = await item.findOne({ where: { barcode_var } });
     
     if (!result) {
-      // Return 404 if no item was found with the given barcode number
       return res.status(404).send('Item not found');
     }
     
-    // Return the item object if it exists
     return res.send(result);
   });
   
