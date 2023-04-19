1abbebcee4e9052e10d8d76d0527d47ad2621353
Sathya <svvsathyanarayanan@gmail.com>
2023-04-19 12:55:41 -0700

    remove onloadend events on request

--- frontend/src/mirage/index.ts
+++ frontend/src/mirage/index.ts
@@ -7,6 +7,16 @@ export function makeServer({ environment = "test" }) {
   return createServer({
     environment,
     routes() {
+      // Axios and MirageJS issue fixed based on comment https://github.com/pretenderjs/pretender/issues/354#issuecomment-1299297701
+      const NativeXMLHttpRequest = window.XMLHttpRequest;
+
+      (window as any).XMLHttpRequest = function XMLHttpRequest() {
+        const request: any = new NativeXMLHttpRequest();
+        delete request.onloadend;
+        return request;
+      };
+      // End fix
+
       this.namespace = "api";
       this.urlPrefix = "http://localhost:3001";
       getAuthRoutes(this);
