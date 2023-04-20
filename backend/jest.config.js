c80beda8d349dafb95441362a048b14a975e6163
sourabhk25 <sourabhkulkarni258@gmail.com>
2023-04-20 11:40:38 -0700

    Coverage report config added for backend

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -6,4 +6,15 @@ module.exports = {
     //"\\.pc/",
     // "/frontend/",
   ],
+  collectCoverage: true,
+  coverageDirectory: 'src/__tests__/coverage',
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 };
