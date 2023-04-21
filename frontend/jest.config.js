789c608ee943e89aab543345e56356e72b79bd15
sourabhk25 <sourabhkulkarni258@gmail.com>
2023-04-20 17:16:29 -0700

    Reverted testEnvironment for frontend to jsdom

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,8 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  // testEnvironment: 'jsdom',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
     '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
