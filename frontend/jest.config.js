40db2af27f237fe0f353a22d657d21a689104eba
Anoushka Sharma <anoushka444@gmail.com>
2023-04-21 13:23:50 -0700

    Configured test and coverage script for FE

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -14,13 +14,5 @@ module.exports = {
     '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
   ],
   collectCoverage: true,
-  verbose: true,
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  }
+  verbose: true
 };
