4e41cab0615471d39e3d623cf37a4dfda69fc6ab
chetan2298 <45332082+chetan2298@users.noreply.github.com>
2023-04-21 03:49:03 -0700

    Added Dialog box Check for Add New Entry

--- frontend/src/__tests__/donor.test.tsx
+++ frontend/src/__tests__/donor.test.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+//Alphabetical Order imports for better readability
+import '@testing-library/jest-dom'
 import Donor from '../pages/donor';
+import { render, fireEvent, screen } from '@testing-library/react';
+import React from 'react';
 
 describe('Donor Page', () => {
-
-    it('should render a button with text "Add Donate"', () => {
+    it('Should render a button with text "Add Donate"', () => {
         render(<Donor />);
         const AddDonor = screen.getByText('Add Donor');
         expect(AddDonor).toBeInTheDocument(); // assert that the button is in the document
+    });
+    it("Displays a dialog box for adding a new donor when the 'Add Donor' button is clicked", () => {
+        const { getByRole, queryByText } = render(<Donor />);
+        const addButton = getByRole("button", { name: "Add Donor" });
+        fireEvent.click(addButton);
+        expect(queryByText("Add New Entry")).toBeInTheDocument();
     });
 });
