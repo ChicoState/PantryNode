850dc87e8bb4bf6ce74c16b9aa10356c750d8eac
Manavi Ghorpade <101484018+Manavi-ghorpade@users.noreply.github.com>
2023-04-30 00:07:44 -0700

    Update EmailVerify.test.tsx

--- frontend/src/__tests__/EmailVerify.test.tsx
+++ frontend/src/__tests__/EmailVerify.test.tsx
@@ -7,7 +7,9 @@ import { MemoryRouter } from "react-router";
 import { store } from "../store";
 import { Provider } from "react-redux";
 
+
 describe('EmailResetComponent', () => {
+// This test case checks if the component is rendered with form fields
   test('renders the component with form fields', () => {
     render(
             <Provider store={store}>
@@ -21,7 +23,8 @@ describe('EmailResetComponent', () => {
     const getCodeButton = screen.getByRole('button', { name: 'Get Code' });
     expect(getCodeButton).toBeInTheDocument();
   });
-
+  
+    // This test case checks if an error message is displayed when an invalid email is entered
     test('displays error message when email is invalid', () => {
         render(
             <Provider store={store}>
@@ -37,7 +40,7 @@ describe('EmailResetComponent', () => {
         const errorMessage = screen.getByText('Email is invalid');
         expect(errorMessage).toBeInTheDocument();
     });
-    
+    // This test case checks if an error message is displayed when no email is entered
     test('displays error message when email is empty', () => {
         render(
             <Provider store={store}>
@@ -49,7 +52,7 @@ describe('EmailResetComponent', () => {
         const getCodeButton = screen.getByRole('button', { name: 'Get Code' });
         fireEvent.click(getCodeButton);
     });
-    
+    // This test case checks if the user is navigated to the verify page when a valid email is entered
     test('navigates to verify page when email is valid', () => {
         render(
             <Provider store={store}>
