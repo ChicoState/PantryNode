091d847b7b5469bde4d7fae78de38b1d7a0b8e14
Manohar Mendan <mendan.m@gmail.com>
2023-05-13 17:25:20 -0700

    Added test case for Header

--- frontend/src/__tests__/Header.test.tsx
+++ frontend/src/__tests__/Header.test.tsx
@@ -1,33 +1,43 @@
-import { Button, Stack, Typography } from "@mui/material";
-import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
-import React from "react";
+import React from 'react';
+import { render, fireEvent } from "@testing-library/react";
+import Header from "../Components/Quagga/Header";
 
-// eslint-disable-next-line react/prop-types
-const Header = ({ cameraStatus, handleToggleCamera }) => {
-  return (
-    <Stack
-      sx={{
-        flexDirection: "row",
-        justifyContent: "space-between",
-        width: "100%",
-        alignItems: "center",
-        marginBottom: 7,
-        flexWrap: "wrap",
-      }}>
+describe("Header component", () => {
+  test("renders the title 'Scan Now'", () => {
+    const { getByText } = render(<Header />);
+    const title = getByText(/Scan Now/i);
+    expect(title).toBeInTheDocument();
+  });
 
-      <Typography variant="h4" fontWeight="bold" color="primary">
-        Scan Now
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleToggleCamera}
-        sx={{ paddingRight: 2, marginY: "auto", alignSelf: "flex-end" }}>
-        {<QrCodeScannerIcon sx={{ marginRight: 1 }} />}
-        {cameraStatus ? "Stop" : "Start"} Camera
-      </Button>
-    </Stack>
-  );
-};
+  test("renders the 'Start Camera' button", () => {
+    const { getByRole } = render(<Header />);
+    const button = getByRole("button", { name: /Start Camera/i });
+    expect(button).toBeInTheDocument();
+  });
 
-export default Header;
+  test("clicking the 'Start Camera' button calls the handleToggleCamera function", () => {
+    const mockHandleToggleCamera = jest.fn();
+    const { getByRole } = render(
+      <Header cameraStatus={false} handleToggleCamera={mockHandleToggleCamera} />
+    );
+    const button = getByRole("button", { name: /Start Camera/i });
+    fireEvent.click(button);
+    expect(mockHandleToggleCamera).toHaveBeenCalledTimes(1);
+  });
+
+  test("displays the 'Stop Camera' button when cameraStatus is true", () => {
+    const { getByRole } = render(<Header cameraStatus={true} />);
+    const button = getByRole("button", { name: /Stop Camera/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  test("clicking the 'Stop Camera' button calls the handleToggleCamera function", () => {
+    const mockHandleToggleCamera = jest.fn();
+    const { getByRole } = render(
+      <Header cameraStatus={true} handleToggleCamera={mockHandleToggleCamera} />
+    );
+    const button = getByRole("button", { name: /Stop Camera/i });
+    fireEvent.click(button);
+    expect(mockHandleToggleCamera).toHaveBeenCalledTimes(1);
+  });
+});
