1357c9b9d3fa6f4b9218c2ddcc2153326ea2df19
Nidhi Kulkarni <nidhikulkarni@Nidhis-MacBook-Air.local>
2023-05-14 00:27:37 -0700

    Donnor additional coverage

--- frontend/src/__tests__/donors.test.tsx
+++ frontend/src/__tests__/donors.test.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import userEvent from "@testing-library/user-event";
+import { Dialog } from "@material-ui/core";
+
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+
 import { useNavigate } from "react-router-dom";
 import Donor from "../pages/donors";
 
@@ -37,7 +42,7 @@ describe("Donor component", () => {
         );
         const addDonorButton = getByRole("button", { name: "Add Donor" });
         fireEvent.click(addDonorButton);
-        const personID = getByLabelText("Person ID")
+        const personID = getByLabelText("Person ID");
         const emailInput = getByLabelText("Email");
         const addButton = getByText("Add");
         expect(addButton).toBeDisabled();
@@ -68,4 +73,89 @@ describe("Donor component", () => {
         expect(queryByText("Email is invalid")).toBeInTheDocument();
     });
 
+    it("opens the Add Donor dialog when the 'Add Donor' button is clicked", () => {
+        const navigate = jest.fn();
+        (useNavigate as jest.Mock).mockReturnValue(navigate);
+        const { getByRole, getByLabelText } = render(<Donor />);
+        const addDonorButton = getByRole("button", { name: "Add Donor" });
+        fireEvent.click(addDonorButton);
+        const emailInput = getByLabelText("Email");
+        expect(emailInput).toBeInTheDocument();
+      });
+
+     it("opens the Lookup Donor dialog when the 'Lookup Donor' button is clicked:Person ID", () => {
+  const navigate = jest.fn();
+  (useNavigate as jest.Mock).mockReturnValue(navigate);
+  const { getByRole, getByText, getByLabelText } = render(<Donor />);
+  const lookupDonorButton = getByText("Lookup Donor");
+  fireEvent.click(lookupDonorButton);
+  const personIDInput = getByLabelText("Person ID");
+  expect(personIDInput).toBeInTheDocument();
+});
+
+it("calls the navigate function with the correct route when the 'Add' button is clicked in the 'Add Donor' dialog", async () => {
+    const navigate = jest.fn();
+    (useNavigate as jest.Mock).mockReturnValue(navigate);
+    const { getByRole, getByLabelText, getByText } = render(<Donor />);
+    const addDonorButton = getByRole("button", { name: "Add Donor" });
+    fireEvent.click(addDonorButton);
+    const personID = getByLabelText("Person ID");
+    const emailInput = getByLabelText("Email");
+    const addButton = getByText("Add");
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+    fireEvent.change(personID, { target: { value: "1" } });
+    fireEvent.click(addButton);
+  });
+  it("displays an error message if the no email address is provided in the Email Address field of the add donor dialog", async () => {
+    const navigate = jest.fn();
+    (useNavigate as jest.Mock).mockReturnValue(navigate);
+    const { getByRole, getByLabelText, getByText, queryByText } = render(
+        <Donor />
+    );
+    const addDonorButton = getByRole("button", { name: "Add Donor" });
+    fireEvent.click(addDonorButton);
+    const personID = getByLabelText("Person ID");
+    const emailInput = getByLabelText("Email");
+    const addButton = getByText("Add");
+    expect(addButton).toBeDisabled();
+    //First we put in dummy text
+    fireEvent.change(emailInput, { target: { value: "zyzz" } });
+    fireEvent.change(personID, { target: { value: "2" } });
+    //Removing the dummy text to initiate warning message
+    fireEvent.change(emailInput, { target: { value: "" } });
+    fireEvent.click(addButton);
+    await waitFor(() => expect(queryByText("Email is required")).toBeInTheDocument());
+  });
+  
+  test("submits lookup form when Lookup button is clicked in the Lookup Donor dialog", async () => {
+    const { getByRole, getByLabelText, getByText } = render(<Donor />);
+    const lookupDonorButton = getByRole("button", { name: "Lookup Donor" });
+    fireEvent.click(lookupDonorButton);
+
+    const personID = getByLabelText("Person ID");
+    const fullName = getByLabelText("Full Name");
+    const lookupButton = getByText("Lookup");
+
+    fireEvent.change(personID, { target: { value: "1" } });
+    fireEvent.change(fullName, { target: { value: "Test User" } });
+    fireEvent.click(lookupButton);
+
+    await act(async () => {
+      expect(personID).toHaveValue("1");
+      expect(fullName).toHaveValue("Test User");
+    });
+  });
+
+  test("renders donor list header with two buttons", () => {
+    const { getByRole } = render(<Donor />);
+    const donorListHeader = getByRole("heading", { name: "Donor List" });
+    const addDonorButton = getByRole("button", { name: "Add Donor" });
+    const lookupDonorButton = getByRole("button", { name: "Lookup Donor" });
+
+    expect(donorListHeader).toBeInTheDocument();
+    expect(addDonorButton).toBeInTheDocument();
+    expect(lookupDonorButton).toBeInTheDocument();
+  });
+
+ 
 });
